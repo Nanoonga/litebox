58ba2a07841512e65033e3652ecbe2c5e8b44248
Gary Royal <glroyal@protonmail.com>
2025-03-24 07:41:09 -0500

    year no longer 2024

--- litebox.js
+++ litebox.js
@@ -2,7 +2,7 @@
  *
  *   LiteBox
  *   An Adaptive Density Graphical Photo Browser written in Computed HTML
- *   © Copyright Gary Royal 2022, 2024
+ *   © Copyright Gary Royal 2022, 2025
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; either version 2 of the License, or
